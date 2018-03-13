8e112030851043d1d49f34265c45f0cb659abfb8
wwwjk365 <wwwjk365@gmail.com>
2018-03-13 00:31:01 -0400

    03130029

---
+++ test1.R
@@ -0,0 +1,12 @@
+library(pbdMPI, quiet = TRUE)
+init()
+
+n.gbd <- sample(1:10, size = 1)
+
+sm <- allreduce(n.gbd) # default op is ’sum ’
+print(sm)
+
+gt <- allgather(n.gbd)
+print(gt)
+
+finalize()
