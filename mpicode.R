da638a8ea8bb7cb069cecfbf0d0f416474478727
Michael Yan <wwwjk365@gmail.com>
2018-04-01 23:21:05 -0400

    ttrt

--- mpicode.R
+++ mpicode.R
@@ -1,23 +1,20 @@
-suppressMessages(library(pbdDMAT))
+suppressMessages(library(pbdDMAT, quietly = TRUE))
 init.grid()
 
-if (comm.rank() == 0) { # only read on process 0
-  x <- as.matrix(read.csv("diamonds.csv"))[,-1]
-  y <- as.matrix(read.csv("diamonds.csv"))[,1]
-} else {
-  x <- NULL
-  y <- NULL
+{
+  if (comm.rank()==0)
+    A <- matrix(1:25, 5,5)
+  else
+    A <- NULL
 }
 
-dx <- as.ddmatrix(x)
-dy <- as.ddmatrix(y)
+dA <- as.ddmatrix(A, bldim=2) # distribute
 
-model <- lm.fit(dx, dy)
+print(dA)
+comm.print(submatrix(dA), all.rank = TRUE)
 
-dx.new <- as.ddmatrix(matrix(c(2,60,60,5,5,5), nrow = 1, ncol = 6))
-
-pred <- dx.new %*% model$coefficients
-
-comm.cat(paste("\nThe predicted diamond price is:", submatrix(pred), "\n"), quiet=T)
+newA <- redistribute(dA, bldim=2, ICTXT=1)
+print(newA)
+comm.print(submatrix(newA), all.rank = TRUE)
 
 finalize()
